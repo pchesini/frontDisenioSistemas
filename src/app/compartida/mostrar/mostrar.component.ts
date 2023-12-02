08cec3f4408ea67b0a73cd0711e6146ea810f93e
Lilith Nix <andromeda0209@gmail.com>
2023-12-02 15:34:52 -0300

    agregué responsividad

--- src/app/compartida/mostrar/mostrar.component.ts
+++ src/app/compartida/mostrar/mostrar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./mostrar.component.css']
 })
 export class MostrarComponent {
-  
+    agregarReunion(){
+      //this.router.navigate(['/componenteJero']);
+    }
 }
+
+
