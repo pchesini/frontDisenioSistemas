6e1e742d416739fb4ccfca8b7a9ded9b188a1e31
Pablo Chesini <pchesini1@gmail.com>
2023-12-02 18:21:08 -0300

    Se agrego  la interface de nueva Reunion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { PaginaPrincipalComponent } from './pages/pagina-principal/pagina-principal.component';
-
+import { NuevaRnComponent }from './pages/nueva-rn/nueva-rn.component';
 //le agrego las rutas
 const routes: Routes = [
   {path:'',redirectTo:'/inicio', pathMatch:'full'},
   {path:'inicio',component:DashboardComponent},
   {path:'iniciar-sesion',component:LoginComponent},
-  {path:'pagina-principal',component:PaginaPrincipalComponent}
+  {path:'pagina-principal',component:PaginaPrincipalComponent},
+  {path:'nueva-reunion',component:NuevaRnComponent},
 ];
   
 
