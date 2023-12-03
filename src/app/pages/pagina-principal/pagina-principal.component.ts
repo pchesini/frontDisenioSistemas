8a375815359e32058a58e4a67abafc3812d12ced
Pablo Chesini <pchesini1@gmail.com>
2023-12-03 14:03:34 -0300

    Se corrigieron rutas

--- src/app/pages/pagina-principal/pagina-principal.component.ts
+++ src/app/pages/pagina-principal/pagina-principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/services/auth/Usuario';
+import { LoginService } from 'src/app/services/auth/login.service';
 
 @Component({
   selector: 'app-pagina-principal',
@@ -7,7 +9,10 @@ import { Component } from '@angular/core';
 })
 export class PaginaPrincipalComponent {
   cuadroVisible: string | null;  // Inicializar con null o algún otro valor inicial
-  constructor() {
+  //se agrego la funcionalidad de cerrar sesion
+  usuarioLogeado:boolean = false;
+userData?:Usuario;
+  constructor(private loginService: LoginService) {
     this.cuadroVisible = null;
   }
   mostrarCuadro(idCuadro: string) {
@@ -15,4 +20,9 @@ export class PaginaPrincipalComponent {
     this.cuadroVisible = this.cuadroVisible === idCuadro ? null : idCuadro;
     console.log(`Nuevo valor de cuadroVisible: ${this.cuadroVisible}`);
   }
+  ngOnDestroy(): void {
+    this.loginService.currentUserData.unsubscribe();
+    this.loginService.currentUserLoginOn.unsubscribe();
 }
+}
+
